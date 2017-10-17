eb568435ea1394f11057d26466122318851adca1
BT_1N3_02 <BT_1N3_02@BT_1N3_02.bktech.local>
2017-10-17 15:33:31 -0400

    update

--- src/arrays/ArraysMain.java
+++ src/arrays/ArraysMain.java
@@ -8,6 +8,128 @@ public class ArraysMain {
 	private String[] values;
 	
 	public ArraysMain() {
+		tuesdayMethods();
+	}
+	
+	private void tuesdayMethods() {
+		int[] orderTest = {1, 2, 3, 4, 5, 1, 6, 7, 8, 9, 10, 11};
+//		cycleThrough(orderTest, 3);
+//		System.out.println(Arrays.toString(orderTest));
+		System.out.println(longestConsecutiveSequence(orderTest) + "  is longest LCS.");
+		
+	}
+	
+	/**
+	 * return the length of the longest cons. sequence in the array
+	 * for example:
+	 * LCS({1, 2, 3, 2, 3, 4, 5, 2, 3 , 4}) -> 4
+	 * @param arr
+	 * @return
+	 */
+	private int longestConsecutiveSequence(int[] arr) {
+		int maxLength = 1;
+		int currentCount = 1;
+		for(int i  = 0; i < arr.length; i++) {
+			while (i+ currentCount < arr.length && isConsecutive(arr, i , i+currentCount)) {
+				currentCount++;
+			}
+			if (currentCount > maxLength) {
+				maxLength = currentCount;
+			}
+			i = i + currentCount-1;
+			currentCount = 1;
+		}
+		return maxLength;
+		/*
+		int tempCount = 0;
+		int recentIndex = 0;
+		for(int i = 0; i < arr.length; i++) {
+			if (ifConsecutive(i, i+1)) {
+				tempCount++;
+			}
+			if(tempCount > totalCount) {
+				totalCount = tempCount;
+			}
+		}
+		return totalCount;
+		*/
+	}
+	
+	private boolean ifConsecutive(int x,int y) {
+		if (x + 1 == y) {
+			return true;
+		}
+		return false;
+	}
+	
+	/**
+	 * returns true if all the elements from start to end are increasing by 1
+	 * example:
+	 * iC ({1, 7, 3, 6, 7, 8, 12} 3, 5) -> true
+	 * @param arr
+	 * @param start
+	 * @param end
+	 * @return
+	 */
+	
+	private boolean isConsecutive(int[] arr, int start, int end) {
+		for(int i = start; i <= end; i++) {
+			if(arr[i] +1 != arr[i+1]) {
+				return false;
+			}
+		}
+		return true;
+	}
+	
+	/**
+	 * the element at index 0 moves to the last position in the array 
+	 * as all other elements move forward. this must happen exactly n times
+	 * @param orderTest
+	 * @param n
+	 */
+	private void cycleThrough(int[] arr, int n) {
+		for (int i = 0; i < n; i++) {
+			frontToBack(arr);
+		}
+	}
+
+	/**
+	 * removes the element at index 0, pushes all the other elements
+	 * forward: 1 goes to 0, 2 goes to 1 ...
+	 * puts the element that was index 0 at end
+	 * @param arr
+	 * @return
+	 */
+	private void frontToBack(int[] arr) {
+		int x = arr[0];
+		for(int i = 0; i < arr.length-1; i++) {
+			arr[i] = arr[i+1];
+		}
+		arr[arr.length-1] = x;
+	}
+
+	private void warmUpMethods() {
+		int[] orderTest = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+		reverseOrder(orderTest);
+		System.out.println(Arrays.toString(orderTest));
+		System.out.println(Arrays.toString(subArray(orderTest, 3, 4)));
+	}
+
+	void reverseOrder(int[] arr) {
+		for(int i = 0; i < arr.length/2; i++) { //divide 2?
+			swap(arr, i, arr.length-1-i);
+		}
+	}
+	
+	int[] subArray(int[] arr, int psn, int length) {
+		int[] sub = new int[length]; 
+		for(int i = 0; i < length; i++) {
+			sub[i] = arr[i+psn];
+		}
+		return sub;
+	}
+	
+	public void cardMethods() {
 		suits = new String[4];
 		suits[0] = "Clubs";
 		suits[1] = "Hearts";
