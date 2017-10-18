22d0f0326d2c1a893303c60910fc834793596869
BT_1N3_02 <BT_1N3_02@BT_1N3_02.bktech.local>
2017-10-18 15:33:21 -0400

    update classes object

--- src/arrays/Person.java
+++ src/arrays/Person.java
@@ -19,4 +19,9 @@ public class Person {
 		this.lastName = last;
 		this.home = home;
 	}
+	
+	public String toString() {
+		return "My name is " + firstName + " " + 
+				lastName + " and I am from " + home + ".";
+	}
 }
