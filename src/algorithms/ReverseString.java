6a45ab746cd483320202ef740259e0e0143974fd
BT_1N3_08 <BT_1N3_08@BT_1N3_08.bktech.local>
2017-12-04 15:30:46 -0500

    c

---
+++ src/algorithms/ReverseString.java
@@ -0,0 +1,30 @@
+package algorithms;
+
+public class ReverseString {
+	private static String reverse = "";
+	
+	public static void main(String[] args) {
+		reverseString("example");
+	}
+	 
+	public static String reverseString(String str) {
+		if(str.length() == 1) {
+			reverse += str;
+		}else {
+			reverse += str.substring(str.length() -1, str.length()) +
+					reverseString(str.substring(0, str.length()-1));
+		}
+		return reverse;
+	}
+	
+	//str.substring(1)) + str.charAt(0
+	/*
+	public int length() {
+		return str.length();
+	}
+	
+	public String substring(int beginIndex, int endIndex) {
+		return str.substring(beginIndex, endIndex);
+	}	
+	*/
+}
